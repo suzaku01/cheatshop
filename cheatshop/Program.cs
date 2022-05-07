b691c835212e8000bdedc97a5337fd4a906111fb
suzaku <89909040+suzaku01@users.noreply.github.com>
2022-05-08 07:27:09 +0900

    update

--- cheatshop/Program.cs
+++ cheatshop/Program.cs
@@ -1,302 +1,240 @@
-﻿myfunc();
-
-void myfunc()
+﻿class Program
 {
-    byte[] by = File.ReadAllBytes(args[0]);
-    var li = new List<byte>();
-    li = by.ToList();
-    int po1 = BitConverter.ToInt32(by, 196);
-    int po2 = BitConverter.ToInt32(by, po1 + 12);   //list
-    int po3 = BitConverter.ToInt32(by, po1 + 28);
-
-    int katate = po2;
-    int dual = katate + 4;
-    int great = dual + 4;
-    int longs = great + 4;
-    int hammer = longs + 4;
-    int fue = hammer + 4;
-    int lance = fue + 4;
-    int gance = lance + 4;
-    int light = gance + 4;
-    int heavy = light + 4;
-    int bow = heavy + 4;
-
-    int head = po2 + 44;
-    int chest = po2 + 48;
-    int arms = po2 + +52;
-    int waist = po2 + 56;
-    int legs = po2 + 60;
-    byte[] zero = { 00, 00, 00, 00 };
-
-    var idList = File.ReadAllBytes("data/katate").ToList();
-    byte[] leng = BitConverter.GetBytes(li.Count);
-    li[katate] = leng[0];
-    li[katate + 1] = leng[1];
-    li[katate + 2] = leng[2];
-    li[katate + 3] = leng[3];
-    li[po3] = leng[0];
-    li[po3 + 1] = leng[1];
-    li[po3 + 2] = leng[2];
-    li[po3 + 3] = leng[3];
-    li.AddRange(idList);
-
-    idList = File.ReadAllBytes("data/swaxe").ToList();
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-    idList = File.ReadAllBytes("data/dual").ToList();
-    leng = BitConverter.GetBytes(li.Count);
-    li[dual] = leng[0];
-    li[dual + 1] = leng[1];
-    li[dual + 2] = leng[2];
-    li[dual + 3] = leng[3];
-    li[po3 + 4] = leng[0];
-    li[po3 + 5] = leng[1];
-    li[po3 + 6] = leng[2];
-    li[po3 + 7] = leng[3];
-    li.AddRange(idList);
-
-    idList = File.ReadAllBytes("data/tonfa").ToList();
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-
-    idList = File.ReadAllBytes("data/greawsword").ToList();
-    leng = BitConverter.GetBytes(li.Count);
-    li[great] = leng[0];
-    li[great + 1] = leng[1];
-    li[great + 2] = leng[2];
-    li[great + 3] = leng[3];
-    li[po3 + 8] = leng[0];
-    li[po3 + 9] = leng[1];
-    li[po3 + 10] = leng[2];
-    li[po3 + 11] = leng[3];
-    li.AddRange(idList);
-
-    idList = File.ReadAllBytes("data/ryuukon").ToList();
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-    idList = File.ReadAllBytes("data/longsword").ToList();
-    leng = BitConverter.GetBytes(li.Count);
-    li[longs] = leng[0];
-    li[longs + 1] = leng[1];
-    li[longs + 2] = leng[2];
-    li[longs + 3] = leng[3];
-    li[po3 + 12] = leng[0];
-    li[po3 + 13] = leng[1];
-    li[po3 + 14] = leng[2];
-    li[po3 +15] = leng[3];
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-
-    idList = File.ReadAllBytes("data/hammer").ToList();
-    leng = BitConverter.GetBytes(li.Count);
-    li[hammer] = leng[0];
-    li[hammer + 1] = leng[1];
-    li[hammer + 2] = leng[2];
-    li[hammer + 3] = leng[3];
-    li[po3 + 16] = leng[0];
-    li[po3 + 17] = leng[1];
-    li[po3 + 18] = leng[2];
-    li[po3 + 19] = leng[3];
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-    idList = File.ReadAllBytes("data/fue").ToList();
-    leng = BitConverter.GetBytes(li.Count);
-    li[fue] = leng[0];
-    li[fue + 1] = leng[1];
-    li[fue + 2] = leng[2];
-    li[fue + 3] = leng[3];
-    li[po3 + 20] = leng[0];
-    li[po3 + 21] = leng[1];
-    li[po3 + 22] = leng[2];
-    li[po3 + 23] = leng[3];
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-    idList = File.ReadAllBytes("data/lance").ToList();
-    leng = BitConverter.GetBytes(li.Count);
-    li[lance] = leng[0];
-    li[lance + 1] = leng[1];
-    li[lance + 2] = leng[2];
-    li[lance + 3] = leng[3];
-    li[po3 + 24] = leng[0];
-    li[po3 + 25] = leng[1];
-    li[po3 + 26] = leng[2];
-    li[po3 + 27] = leng[3];
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-    idList = File.ReadAllBytes("data/gance").ToList();
-    leng = BitConverter.GetBytes(li.Count);
-    li[gance] = leng[0];
-    li[gance + 1] = leng[1];
-    li[gance + 2] = leng[2];
-    li[gance + 3] = leng[3];
-    li[po3 + 28] = leng[0];
-    li[po3 + 29] = leng[1];
-    li[po3 + 30] = leng[2];
-    li[po3 +31] = leng[3];
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-    idList = File.ReadAllBytes("data/light").ToList();
-    leng = BitConverter.GetBytes(li.Count);
-    li[light] = leng[0];
-    li[light + 1] = leng[1];
-    li[light + 2] = leng[2];
-    li[light + 3] = leng[3];
-    li[po3 +32] = leng[0];
-    li[po3 + 33] = leng[1];
-    li[po3 + 34] = leng[2];
-    li[po3 + 35] = leng[3];
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-    idList = File.ReadAllBytes("data/heavy").ToList();
-    leng = BitConverter.GetBytes(li.Count);
-    li[heavy] = leng[0];
-    li[heavy + 1] = leng[1];
-    li[heavy + 2] = leng[2];
-    li[heavy + 3] = leng[3];
-    li[po3 + 36] = leng[0];
-    li[po3 + 37] = leng[1];
-    li[po3 + 38] = leng[2];
-    li[po3 + 39] = leng[3];
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-    idList = File.ReadAllBytes("data/bow").ToList();
-    leng = BitConverter.GetBytes(li.Count);
-    li[bow] = leng[0];
-    li[bow + 1] = leng[1];
-    li[bow + 2] = leng[2];
-    li[bow + 3] = leng[3];
-    li[po3 + 40] = leng[0];
-    li[po3 + 41] = leng[1];
-    li[po3 + 42] = leng[2];
-    li[po3 + 43] = leng[3];
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-
-    idList = new List<byte>();
-    int ids = 14593;
-    for (int t = 1; t < ids + 1; t++)
+    static void Main(string[] args)
     {
-        byte[] bh = { 0, 0 };
-        byte[] b = BitConverter.GetBytes(t);
-        bh[0] = b[0];
-        bh[1] = b[1];
-        idList.AddRange(bh);
+        var list = new List<byte>();
+        int pointer1 = 1;
+        int pointer2 = 2;
+        int pointer3 = 3;
+        int pointer4 = 4;
+        int pointer5 = 5;
+        int pointer6 = 6;
+        int pointer7 = 7;
+        int pointer8 = 8;
+        bool patched = false;
+
+        while (!patched)
+        {
+            if (args[0].Length > 0)
+            {
+                byte[] by = File.ReadAllBytes(args[0]);
+                if (BitConverter.ToInt32(by, 0) != 442919021)
+                    return;
+
+                list = by.ToList();
+                byte[] devide = { 00, 00, 00, 00 };
+                int increment = 0;
+
+                int firstPointer = BitConverter.ToInt32(by, 196);   //C76D60
+                pointer1 = BitConverter.ToInt32(by, firstPointer + 0);
+                pointer2 = BitConverter.ToInt32(by, firstPointer + 4);
+                pointer3 = BitConverter.ToInt32(by, firstPointer + 8);
+                pointer4 = BitConverter.ToInt32(by, firstPointer + 12);
+                pointer5 = BitConverter.ToInt32(by, firstPointer + 16);
+                pointer6 = BitConverter.ToInt32(by, firstPointer + 20);
+                pointer7 = BitConverter.ToInt32(by, firstPointer + 24);
+                pointer8 = BitConverter.ToInt32(by, firstPointer + 28);
+
+                byte[] data = File.ReadAllBytes("data/1_katate");
+                byte[] length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment, length);
+                list.AddRange(data);
+
+                data = File.ReadAllBytes("data/12_slash");
+                list.AddRange(data);
+                list.AddRange(devide);
+
+                data = File.ReadAllBytes("data/2_dual");
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(data);
+
+                data = File.ReadAllBytes("data/13_tonfa");
+                list.AddRange(data);
+                list.AddRange(devide);
+
+                data = File.ReadAllBytes("data/3_great");
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(data);
+
+                data = File.ReadAllBytes("data/14_senryuu");
+                list.AddRange(data);
+                list.AddRange(devide);
+
+                data = File.ReadAllBytes("data/4_tachi");
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(data);
+                list.AddRange(devide);
+
+                data = File.ReadAllBytes("data/5_hammer");
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(data);
+                list.AddRange(devide);
+
+                data = File.ReadAllBytes("data/6_horn");
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(data);
+                list.AddRange(devide);
+
+                data = File.ReadAllBytes("data/7_lance");
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(data);
+                list.AddRange(devide);
+
+                data = File.ReadAllBytes("data/8_gance");
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(data);
+                list.AddRange(devide);
+
+                data = File.ReadAllBytes("data/9_light");
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(data);
+                list.AddRange(devide);
+
+                data = File.ReadAllBytes("data/10_heavy");
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(data);
+                list.AddRange(devide);
+
+                data = File.ReadAllBytes("data/11_bow");
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(data);
+                list.AddRange(devide);
+
+                var idlist = new List<byte>();
+                int idCount = 14593;
+                for (int t = 1; t < idCount + 1; t++)
+                {
+                    byte[] tempBytes = { 00, 00 };
+                    byte[] id = BitConverter.GetBytes(t);
+                    tempBytes[0] = id[0];
+                    tempBytes[1] = id[1];
+                    idlist.AddRange(tempBytes);
+                }
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(idlist);
+                list.AddRange(devide);
+
+                idlist = new List<byte>();
+                idCount = 13461;
+                for (int t = 1; t < idCount + 1; t++)
+                {
+                    byte[] tempBytes = { 00, 00 };
+                    byte[] id = BitConverter.GetBytes(t);
+                    tempBytes[0] = id[0];
+                    tempBytes[1] = id[1];
+                    idlist.AddRange(tempBytes);
+                }
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(idlist);
+                list.AddRange(devide);
+
+                idlist = new List<byte>();
+                idCount = 13451;
+                for (int t = 1; t < idCount + 1; t++)
+                {
+                    byte[] tempBytes = { 00, 00 };
+                    byte[] id = BitConverter.GetBytes(t);
+                    tempBytes[0] = id[0];
+                    tempBytes[1] = id[1];
+                    idlist.AddRange(tempBytes);
+                }
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(idlist);
+                list.AddRange(devide);
+
+                idlist = new List<byte>();
+                idCount = 13707;
+                for (int t = 1; t < idCount + 1; t++)
+                {
+                    byte[] tempBytes = { 00, 00 };
+                    byte[] id = BitConverter.GetBytes(t);
+                    tempBytes[0] = id[0];
+                    tempBytes[1] = id[1];
+                    idlist.AddRange(tempBytes);
+                }
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(idlist);
+                list.AddRange(devide);
+
+                idlist = new List<byte>();
+                idCount = 13513;
+                for (int t = 1; t < idCount + 1; t++)
+                {
+                    byte[] tempBytes = { 00, 00 };
+                    byte[] id = BitConverter.GetBytes(t);
+                    tempBytes[0] = id[0];
+                    tempBytes[1] = id[1];
+                    idlist.AddRange(tempBytes);
+                }
+                length = BitConverter.GetBytes(list.Count);
+                WriteLength(increment = increment + 4, length);
+                list.AddRange(idlist);
+                list.AddRange(devide);
+
+
+
+                File.WriteAllBytes("mhfdat.bin", list.ToArray());
+                Console.WriteLine("Patch completed.");
+                patched = true;
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("end.");
+            }
+
+        }
+
+        void WriteLength(int add, byte[] length)
+        {
+            list[pointer1 + add] = length[0];
+            list[pointer1 + add + 1] = length[1];
+            list[pointer1 + add + 2] = length[2];
+            list[pointer1 + add + 3] = length[3];
+            list[pointer2 + add] = length[0];
+            list[pointer2 + add + 1] = length[1];
+            list[pointer2 + add + 2] = length[2];
+            list[pointer2 + add + 3] = length[3];
+            list[pointer3 + add] = length[0];
+            list[pointer3 + add + 1] = length[1];
+            list[pointer3 + add + 2] = length[2];
+            list[pointer3 + add + 3] = length[3];
+            list[pointer4 + add] = length[0];
+            list[pointer4 + add + 1] = length[1];
+            list[pointer4 + add + 2] = length[2];
+            list[pointer4 + add + 3] = length[3];
+            list[pointer5 + add] = length[0];
+            list[pointer5 + add + 1] = length[1];
+            list[pointer5 + add + 2] = length[2];
+            list[pointer5 + add + 3] = length[3];
+            list[pointer6 + add] = length[0];
+            list[pointer6 + add + 1] = length[1];
+            list[pointer6 + add + 2] = length[2];
+            list[pointer6 + add + 3] = length[3];
+            list[pointer7 + add] = length[0];
+            list[pointer7 + add + 1] = length[1];
+            list[pointer7 + add + 2] = length[2];
+            list[pointer7 + add + 3] = length[3];
+            list[pointer8 + add] = length[0];
+            list[pointer8 + add + 1] = length[1];
+            list[pointer8 + add + 2] = length[2];
+            list[pointer8 + add + 3] = length[3];
+        }
     }
-    leng = BitConverter.GetBytes(li.Count);
-    li[head] = leng[0];
-    li[head + 1] = leng[1];
-    li[head + 2] = leng[2];
-    li[head + 3] = leng[3];
-    li[po3 + 44] = leng[0];
-    li[po3 + 45] = leng[1];
-    li[po3 + 46] = leng[2];
-    li[po3 + 47] = leng[3];
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-
-    idList = new List<byte>();
-    ids = 13461;
-    for (int t = 1; t < ids + 1; t++)
-    {
-        byte[] bh = { 0, 0 };
-        byte[] b = BitConverter.GetBytes(t);
-        bh[0] = b[0];
-        bh[1] = b[1];
-        idList.AddRange(bh);
-    }
-    leng = BitConverter.GetBytes(li.Count);
-    li[chest] = leng[0];
-    li[chest + 1] = leng[1];
-    li[chest + 2] = leng[2];
-    li[chest + 3] = leng[3];
-    li[po3 + 48] = leng[0];
-    li[po3 +49] = leng[1];
-    li[po3 + 50] = leng[2];
-    li[po3 + 51] = leng[3];
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-    idList = new List<byte>();
-    ids = 13451;
-    for (int t = 1; t < ids + 1; t++)
-    {
-        byte[] bh = { 0, 0 };
-        byte[] b = BitConverter.GetBytes(t);
-        bh[0] = b[0];
-        bh[1] = b[1];
-        idList.AddRange(bh);
-    }
-    leng = BitConverter.GetBytes(li.Count);
-    li[arms] = leng[0];
-    li[arms + 1] = leng[1];
-    li[arms + 2] = leng[2];
-    li[arms + 3] = leng[3];
-    li[po3 + 52] = leng[0];
-    li[po3 + 53] = leng[1];
-    li[po3 +54] = leng[2];
-    li[po3 + 55] = leng[3];
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-
-    //wasit
-    idList = new List<byte>();
-    ids = 13707;
-    for (int t = 1; t < ids + 1; t++)
-    {
-        byte[] bh = { 0, 0 };
-        byte[] b = BitConverter.GetBytes(t);
-        bh[0] = b[0];
-        bh[1] = b[1];
-        idList.AddRange(bh);
-    }
-    leng = BitConverter.GetBytes(li.Count);
-    li[waist] = leng[0];
-    li[waist + 1] = leng[1];
-    li[waist + 2] = leng[2];
-    li[waist + 3] = leng[3];
-    li[po3 + 56] = leng[0];
-    li[po3 + 57] = leng[1];
-    li[po3 + 58] = leng[2];
-    li[po3 + 59] = leng[3];
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-    idList = new List<byte>();
-    ids = 13513;
-    for (int t = 1; t < ids + 1; t++)
-    {
-        byte[] bh = { 0, 0 };
-        byte[] b = BitConverter.GetBytes(t);
-        bh[0] = b[0];
-        bh[1] = b[1];
-        idList.AddRange(bh);
-    }
-    leng = BitConverter.GetBytes(li.Count);
-    li[legs] = leng[0];
-    li[legs + 1] = leng[1];
-    li[legs + 2] = leng[2];
-    li[legs + 3] = leng[3];
-    li[po3 + 60] = leng[0];
-    li[po3 + 61] = leng[1];
-    li[po3 + 62] = leng[2];
-    li[po3 + 63] = leng[3];
-    li.AddRange(idList);
-    li.AddRange(zero);
-
-
-    by = li.ToArray();
-    File.WriteAllBytes("mhfdat.bin", by);
 }
+
+
