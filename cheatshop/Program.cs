97f0212d3281ee84036149396c84dc268e7c2cfa
suzaku <89909040+suzaku01@users.noreply.github.com>
2022-05-07 13:13:46 +0900

    update

--- cheatshop/Program.cs
+++ cheatshop/Program.cs
@@ -7,6 +7,7 @@ void myfunc()
     li = by.ToList();
     int po1 = BitConverter.ToInt32(by, 196);
     int po2 = BitConverter.ToInt32(by, po1 + 12);   //list
+    int po3 = BitConverter.ToInt32(by, po1 + 28);
 
     int katate = po2;
     int dual = katate + 4;
@@ -33,6 +34,10 @@ void myfunc()
     li[katate + 1] = leng[1];
     li[katate + 2] = leng[2];
     li[katate + 3] = leng[3];
+    li[po3] = leng[0];
+    li[po3 + 1] = leng[1];
+    li[po3 + 2] = leng[2];
+    li[po3 + 3] = leng[3];
     li.AddRange(idList);
 
     idList = File.ReadAllBytes("data/swaxe").ToList();
@@ -45,6 +50,10 @@ void myfunc()
     li[dual + 1] = leng[1];
     li[dual + 2] = leng[2];
     li[dual + 3] = leng[3];
+    li[po3 + 4] = leng[0];
+    li[po3 + 5] = leng[1];
+    li[po3 + 6] = leng[2];
+    li[po3 + 7] = leng[3];
     li.AddRange(idList);
 
     idList = File.ReadAllBytes("data/tonfa").ToList();
@@ -58,6 +67,10 @@ void myfunc()
     li[great + 1] = leng[1];
     li[great + 2] = leng[2];
     li[great + 3] = leng[3];
+    li[po3 + 8] = leng[0];
+    li[po3 + 9] = leng[1];
+    li[po3 + 10] = leng[2];
+    li[po3 + 11] = leng[3];
     li.AddRange(idList);
 
     idList = File.ReadAllBytes("data/ryuukon").ToList();
@@ -70,6 +83,10 @@ void myfunc()
     li[longs + 1] = leng[1];
     li[longs + 2] = leng[2];
     li[longs + 3] = leng[3];
+    li[po3 + 12] = leng[0];
+    li[po3 + 13] = leng[1];
+    li[po3 + 14] = leng[2];
+    li[po3 +15] = leng[3];
     li.AddRange(idList);
     li.AddRange(zero);
 
@@ -80,6 +97,10 @@ void myfunc()
     li[hammer + 1] = leng[1];
     li[hammer + 2] = leng[2];
     li[hammer + 3] = leng[3];
+    li[po3 + 16] = leng[0];
+    li[po3 + 17] = leng[1];
+    li[po3 + 18] = leng[2];
+    li[po3 + 19] = leng[3];
     li.AddRange(idList);
     li.AddRange(zero);
 
@@ -89,6 +110,10 @@ void myfunc()
     li[fue + 1] = leng[1];
     li[fue + 2] = leng[2];
     li[fue + 3] = leng[3];
+    li[po3 + 20] = leng[0];
+    li[po3 + 21] = leng[1];
+    li[po3 + 22] = leng[2];
+    li[po3 + 23] = leng[3];
     li.AddRange(idList);
     li.AddRange(zero);
 
@@ -98,6 +123,10 @@ void myfunc()
     li[lance + 1] = leng[1];
     li[lance + 2] = leng[2];
     li[lance + 3] = leng[3];
+    li[po3 + 24] = leng[0];
+    li[po3 + 25] = leng[1];
+    li[po3 + 26] = leng[2];
+    li[po3 + 27] = leng[3];
     li.AddRange(idList);
     li.AddRange(zero);
 
@@ -107,6 +136,10 @@ void myfunc()
     li[gance + 1] = leng[1];
     li[gance + 2] = leng[2];
     li[gance + 3] = leng[3];
+    li[po3 + 28] = leng[0];
+    li[po3 + 29] = leng[1];
+    li[po3 + 30] = leng[2];
+    li[po3 +31] = leng[3];
     li.AddRange(idList);
     li.AddRange(zero);
 
@@ -116,6 +149,10 @@ void myfunc()
     li[light + 1] = leng[1];
     li[light + 2] = leng[2];
     li[light + 3] = leng[3];
+    li[po3 +32] = leng[0];
+    li[po3 + 33] = leng[1];
+    li[po3 + 34] = leng[2];
+    li[po3 + 35] = leng[3];
     li.AddRange(idList);
     li.AddRange(zero);
 
@@ -125,6 +162,10 @@ void myfunc()
     li[heavy + 1] = leng[1];
     li[heavy + 2] = leng[2];
     li[heavy + 3] = leng[3];
+    li[po3 + 36] = leng[0];
+    li[po3 + 37] = leng[1];
+    li[po3 + 38] = leng[2];
+    li[po3 + 39] = leng[3];
     li.AddRange(idList);
     li.AddRange(zero);
 
@@ -134,6 +175,10 @@ void myfunc()
     li[bow + 1] = leng[1];
     li[bow + 2] = leng[2];
     li[bow + 3] = leng[3];
+    li[po3 + 40] = leng[0];
+    li[po3 + 41] = leng[1];
+    li[po3 + 42] = leng[2];
+    li[po3 + 43] = leng[3];
     li.AddRange(idList);
     li.AddRange(zero);
 
@@ -153,6 +198,10 @@ void myfunc()
     li[head + 1] = leng[1];
     li[head + 2] = leng[2];
     li[head + 3] = leng[3];
+    li[po3 + 44] = leng[0];
+    li[po3 + 45] = leng[1];
+    li[po3 + 46] = leng[2];
+    li[po3 + 47] = leng[3];
     li.AddRange(idList);
     li.AddRange(zero);
 
@@ -172,6 +221,10 @@ void myfunc()
     li[chest + 1] = leng[1];
     li[chest + 2] = leng[2];
     li[chest + 3] = leng[3];
+    li[po3 + 48] = leng[0];
+    li[po3 +49] = leng[1];
+    li[po3 + 50] = leng[2];
+    li[po3 + 51] = leng[3];
     li.AddRange(idList);
     li.AddRange(zero);
 
@@ -190,6 +243,10 @@ void myfunc()
     li[arms + 1] = leng[1];
     li[arms + 2] = leng[2];
     li[arms + 3] = leng[3];
+    li[po3 + 52] = leng[0];
+    li[po3 + 53] = leng[1];
+    li[po3 +54] = leng[2];
+    li[po3 + 55] = leng[3];
     li.AddRange(idList);
     li.AddRange(zero);
 
@@ -210,6 +267,10 @@ void myfunc()
     li[waist + 1] = leng[1];
     li[waist + 2] = leng[2];
     li[waist + 3] = leng[3];
+    li[po3 + 56] = leng[0];
+    li[po3 + 57] = leng[1];
+    li[po3 + 58] = leng[2];
+    li[po3 + 59] = leng[3];
     li.AddRange(idList);
     li.AddRange(zero);
 
@@ -228,6 +289,10 @@ void myfunc()
     li[legs + 1] = leng[1];
     li[legs + 2] = leng[2];
     li[legs + 3] = leng[3];
+    li[po3 + 60] = leng[0];
+    li[po3 + 61] = leng[1];
+    li[po3 + 62] = leng[2];
+    li[po3 + 63] = leng[3];
     li.AddRange(idList);
     li.AddRange(zero);
 
