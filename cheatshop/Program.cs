4399316c504c825bb880ef9b24b56492e2097801
suzaku <89909040+suzaku01@users.noreply.github.com>
2022-05-18 22:31:21 +0900

    update

--- cheatshop/Program.cs
+++ cheatshop/Program.cs
@@ -12,227 +12,194 @@
         int pointer7 = 7;
         int pointer8 = 8;
         bool patched = false;
+        byte[] price = { 255, 255, 255, 255 };
+        byte[] by = File.ReadAllBytes(args[0]);
 
-        while (!patched)
+        int startPointer = 9739572;
+        bool end = false;
+        int readPointer = startPointer;
+        while (!end)
         {
-            if (args[0].Length > 0)
+            if (readPointer > 10906472)
             {
-                byte[] by = File.ReadAllBytes(args[0]);
-                if (BitConverter.ToInt32(by, 0) != 442919021)
-                    return;
-
-                list = by.ToList();
-                byte[] devide = { 00, 00, 00, 00 };
-                int increment = 0;
-
-                int firstPointer = BitConverter.ToInt32(by, 196);   //C76D60
-                pointer1 = BitConverter.ToInt32(by, firstPointer + 0);
-                pointer2 = BitConverter.ToInt32(by, firstPointer + 4);
-                pointer3 = BitConverter.ToInt32(by, firstPointer + 8);
-                pointer4 = BitConverter.ToInt32(by, firstPointer + 12);
-                pointer5 = BitConverter.ToInt32(by, firstPointer + 16);
-                pointer6 = BitConverter.ToInt32(by, firstPointer + 20);
-                pointer7 = BitConverter.ToInt32(by, firstPointer + 24);
-                pointer8 = BitConverter.ToInt32(by, firstPointer + 28);
-
-                byte[] data = File.ReadAllBytes("data/1_katate");
-                byte[] length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment, length);
-                list.AddRange(data);
-
-                data = File.ReadAllBytes("data/12_slash");
-                list.AddRange(data);
-                list.AddRange(devide);
-
-                data = File.ReadAllBytes("data/2_dual");
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(data);
-
-                data = File.ReadAllBytes("data/13_tonfa");
-                list.AddRange(data);
-                list.AddRange(devide);
-
-                data = File.ReadAllBytes("data/3_great");
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(data);
-
-                data = File.ReadAllBytes("data/14_senryuu");
-                list.AddRange(data);
-                list.AddRange(devide);
-
-                data = File.ReadAllBytes("data/4_tachi");
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(data);
-                list.AddRange(devide);
-
-                data = File.ReadAllBytes("data/5_hammer");
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(data);
-                list.AddRange(devide);
-
-                data = File.ReadAllBytes("data/6_horn");
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(data);
-                list.AddRange(devide);
-
-                data = File.ReadAllBytes("data/7_lance");
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(data);
-                list.AddRange(devide);
-
-                data = File.ReadAllBytes("data/8_gance");
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(data);
-                list.AddRange(devide);
-
-                data = File.ReadAllBytes("data/9_light");
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(data);
-                list.AddRange(devide);
-
-                data = File.ReadAllBytes("data/10_heavy");
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(data);
-                list.AddRange(devide);
-
-                data = File.ReadAllBytes("data/11_bow");
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(data);
-                list.AddRange(devide);
-
-                var idlist = new List<byte>();
-                int idCount = 14593;
-                for (int t = 1; t < idCount + 1; t++)
-                {
-                    byte[] tempBytes = { 00, 00 };
-                    byte[] id = BitConverter.GetBytes(t);
-                    tempBytes[0] = id[0];
-                    tempBytes[1] = id[1];
-                    idlist.AddRange(tempBytes);
-                }
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(idlist);
-                list.AddRange(devide);
-
-                idlist = new List<byte>();
-                idCount = 13461;
-                for (int t = 1; t < idCount + 1; t++)
-                {
-                    byte[] tempBytes = { 00, 00 };
-                    byte[] id = BitConverter.GetBytes(t);
-                    tempBytes[0] = id[0];
-                    tempBytes[1] = id[1];
-                    idlist.AddRange(tempBytes);
-                }
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(idlist);
-                list.AddRange(devide);
-
-                idlist = new List<byte>();
-                idCount = 13451;
-                for (int t = 1; t < idCount + 1; t++)
-                {
-                    byte[] tempBytes = { 00, 00 };
-                    byte[] id = BitConverter.GetBytes(t);
-                    tempBytes[0] = id[0];
-                    tempBytes[1] = id[1];
-                    idlist.AddRange(tempBytes);
-                }
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(idlist);
-                list.AddRange(devide);
-
-                idlist = new List<byte>();
-                idCount = 13707;
-                for (int t = 1; t < idCount + 1; t++)
-                {
-                    byte[] tempBytes = { 00, 00 };
-                    byte[] id = BitConverter.GetBytes(t);
-                    tempBytes[0] = id[0];
-                    tempBytes[1] = id[1];
-                    idlist.AddRange(tempBytes);
-                }
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(idlist);
-                list.AddRange(devide);
-
-                idlist = new List<byte>();
-                idCount = 13513;
-                for (int t = 1; t < idCount + 1; t++)
-                {
-                    byte[] tempBytes = { 00, 00 };
-                    byte[] id = BitConverter.GetBytes(t);
-                    tempBytes[0] = id[0];
-                    tempBytes[1] = id[1];
-                    idlist.AddRange(tempBytes);
-                }
-                length = BitConverter.GetBytes(list.Count);
-                WriteLength(increment = increment + 4, length);
-                list.AddRange(idlist);
-                list.AddRange(devide);
-
-
-
-                File.WriteAllBytes("mhfdat.bin", list.ToArray());
-                Console.WriteLine("Patch completed.");
-                patched = true;
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("end.");
+                end = true;
             }
 
+            if (BitConverter.ToInt32(by, readPointer) == 10000000 || BitConverter.ToInt32(by, readPointer) == 5000000)
+            {
+                by[readPointer + 0] = 1;
+                by[readPointer + 1] = 0;
+                by[readPointer + 2] = 0;
+                by[readPointer + 3] = 0;
+            }
+            readPointer = readPointer + 4;
         }
 
-        void WriteLength(int add, byte[] length)
+
+        if (args[0].Length > 0)
         {
-            list[pointer1 + add] = length[0];
-            list[pointer1 + add + 1] = length[1];
-            list[pointer1 + add + 2] = length[2];
-            list[pointer1 + add + 3] = length[3];
-            list[pointer2 + add] = length[0];
-            list[pointer2 + add + 1] = length[1];
-            list[pointer2 + add + 2] = length[2];
-            list[pointer2 + add + 3] = length[3];
-            list[pointer3 + add] = length[0];
-            list[pointer3 + add + 1] = length[1];
-            list[pointer3 + add + 2] = length[2];
-            list[pointer3 + add + 3] = length[3];
-            list[pointer4 + add] = length[0];
-            list[pointer4 + add + 1] = length[1];
-            list[pointer4 + add + 2] = length[2];
-            list[pointer4 + add + 3] = length[3];
-            list[pointer5 + add] = length[0];
-            list[pointer5 + add + 1] = length[1];
-            list[pointer5 + add + 2] = length[2];
-            list[pointer5 + add + 3] = length[3];
-            list[pointer6 + add] = length[0];
-            list[pointer6 + add + 1] = length[1];
-            list[pointer6 + add + 2] = length[2];
-            list[pointer6 + add + 3] = length[3];
-            list[pointer7 + add] = length[0];
-            list[pointer7 + add + 1] = length[1];
-            list[pointer7 + add + 2] = length[2];
-            list[pointer7 + add + 3] = length[3];
-            list[pointer8 + add] = length[0];
-            list[pointer8 + add + 1] = length[1];
-            list[pointer8 + add + 2] = length[2];
-            list[pointer8 + add + 3] = length[3];
+            //by = File.ReadAllBytes(args[0]);
+            //if (BitConverter.ToInt32(by, 0) != 442919021)
+            //    return;
+
+            list = by.ToList();
+            byte[] devide = { 00, 00, 00, 00 };
+            int increment = 0;
+
+            int firstPointer = BitConverter.ToInt32(by, 196);   //C76D60
+            pointer1 = BitConverter.ToInt32(by, firstPointer + 0);
+            pointer2 = BitConverter.ToInt32(by, firstPointer + 4);
+            pointer3 = BitConverter.ToInt32(by, firstPointer + 8);
+            pointer4 = BitConverter.ToInt32(by, firstPointer + 12);
+            pointer5 = BitConverter.ToInt32(by, firstPointer + 16);
+            pointer6 = BitConverter.ToInt32(by, firstPointer + 20);
+            pointer7 = BitConverter.ToInt32(by, firstPointer + 24);
+            pointer8 = BitConverter.ToInt32(by, firstPointer + 28);
+
+            byte[] data = File.ReadAllBytes("data/1_katate");
+            byte[] length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment, length);
+            list.AddRange(data);
+
+            data = File.ReadAllBytes("data/12_slash");
+            list.AddRange(data);
+            list.AddRange(devide);
+
+            data = File.ReadAllBytes("data/2_dual");
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            list.AddRange(data);
+
+            data = File.ReadAllBytes("data/13_tonfa");
+            list.AddRange(data);
+            list.AddRange(devide);
+
+            data = File.ReadAllBytes("data/3_great");
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            list.AddRange(data);
+
+            data = File.ReadAllBytes("data/14_senryuu");
+            list.AddRange(data);
+            list.AddRange(devide);
+
+            data = File.ReadAllBytes("data/4_tachi");
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            list.AddRange(data);
+            list.AddRange(devide);
+
+            data = File.ReadAllBytes("data/5_hammer");
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            list.AddRange(data);
+            list.AddRange(devide);
+
+            data = File.ReadAllBytes("data/6_horn");
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            list.AddRange(data);
+            list.AddRange(devide);
+
+            data = File.ReadAllBytes("data/7_lance");
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            list.AddRange(data);
+            list.AddRange(devide);
+
+            data = File.ReadAllBytes("data/8_gance");
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            list.AddRange(data);
+            list.AddRange(devide);
+
+            data = File.ReadAllBytes("data/9_light");
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            list.AddRange(data);
+            list.AddRange(devide);
+
+            data = File.ReadAllBytes("data/10_heavy");
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            list.AddRange(data);
+            list.AddRange(devide);
+
+            data = File.ReadAllBytes("data/11_bow");
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            list.AddRange(data);
+            list.AddRange(devide);
+
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            byte[] data1 = File.ReadAllBytes("data/a_head.bin");
+            list.AddRange(data1);
+            list.AddRange(devide);
+
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            data1 = File.ReadAllBytes("data/b_chest.bin");
+            list.AddRange(data1);
+            list.AddRange(devide);
+
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            data1 = File.ReadAllBytes("data/c_arms.bin");
+            list.AddRange(data1);
+            list.AddRange(devide);
+
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            data1 = File.ReadAllBytes("data/d_waist.bin");
+            list.AddRange(data1);
+            list.AddRange(devide);
+
+            length = BitConverter.GetBytes(list.Count);
+            WriteLength(increment = increment + 4, length);
+            data1 = File.ReadAllBytes("data/e_legs.bin");
+            list.AddRange(data1);
+            list.AddRange(devide);
+
+            File.WriteAllBytes("mhfdat.bin", list.ToArray());
+            Console.WriteLine("Patch completed.");
+            Console.ReadKey();
+
+            void WriteLength(int add, byte[] length)
+            {
+                list[pointer1 + add] = length[0];
+                list[pointer1 + add + 1] = length[1];
+                list[pointer1 + add + 2] = length[2];
+                list[pointer1 + add + 3] = length[3];
+                list[pointer2 + add] = length[0];
+                list[pointer2 + add + 1] = length[1];
+                list[pointer2 + add + 2] = length[2];
+                list[pointer2 + add + 3] = length[3];
+                list[pointer3 + add] = length[0];
+                list[pointer3 + add + 1] = length[1];
+                list[pointer3 + add + 2] = length[2];
+                list[pointer3 + add + 3] = length[3];
+                list[pointer4 + add] = length[0];
+                list[pointer4 + add + 1] = length[1];
+                list[pointer4 + add + 2] = length[2];
+                list[pointer4 + add + 3] = length[3];
+                list[pointer5 + add] = length[0];
+                list[pointer5 + add + 1] = length[1];
+                list[pointer5 + add + 2] = length[2];
+                list[pointer5 + add + 3] = length[3];
+                list[pointer6 + add] = length[0];
+                list[pointer6 + add + 1] = length[1];
+                list[pointer6 + add + 2] = length[2];
+                list[pointer6 + add + 3] = length[3];
+                list[pointer7 + add] = length[0];
+                list[pointer7 + add + 1] = length[1];
+                list[pointer7 + add + 2] = length[2];
+                list[pointer7 + add + 3] = length[3];
+                list[pointer8 + add] = length[0];
+                list[pointer8 + add + 1] = length[1];
+                list[pointer8 + add + 2] = length[2];
+                list[pointer8 + add + 3] = length[3];
+            }
         }
     }
 }
